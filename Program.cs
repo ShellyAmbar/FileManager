9698bc44e6c716f69b85c3dffb364e859babfd25
Shelly-Ambar <ambar.rachel@gmail.com>
2022-01-12 22:23:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageJsonManager
+{
+    internal class Program
+    {
+        private const string greetingMessage = "Welcome To Package Manager CLI";
+
+        static void Main(string[] args)
+        {
+            //TODO shelly <3
+            // 1. prompt in the terminal the location of "package.json" file path in the system,  and load it to the instance
+            // 2. now ask to update the "version" in the file
+            // 3. save the file
+            // 4. after you done this, add support to package.yaml and to the same thing
+            Console.WriteLine(greetingMessage);
+            Console.WriteLine("Please enter the file name:");
+            string fileName = Console.ReadLine();
+            Console.WriteLine("Please enter the key:");
+            string key = Console.ReadLine();
+            Console.WriteLine("Please enter the value:");
+            string value = Console.ReadLine();
+
+            Console.WriteLine("fileName: {0} ", fileName);
+            Util.UpdateObject(fileName, key, value);
+           
+
+        }
+    }
+}
---
+++ Util.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PackageJsonManager
+{
+    internal class Util
+    {
+       
+
+        public static string GetProjectDirectory() {
+            string workingDirectory = Environment.CurrentDirectory;
+            // or: Directory.GetCurrentDirectory() gives the same result
+            // This will get the current PROJECT directory
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
+            return projectDirectory;
+        }
+        public static void UpdateObject(string fileName, string key , string value)
+        {
+            string filePath = "";
+            string jsonString = "";
+            JObject jsObject = new JObject();
+            if (fileName == null || fileName.Length == 0)
+            {
+                throw new ArgumentNullException("FileName");
+
+            }
+            filePath = Path.Combine(GetProjectDirectory(), fileName);
+
+
+            Console.WriteLine("file full path is : {0}", filePath);
+            if (filePath == null ) { throw new Exception("File not exist"); }
+            jsonString = File.ReadAllText(filePath);
+            try {
+                jsObject = JObject.Parse(jsonString);
+
+                if (jsObject != null)
+                {
+                   
+                    jsObject[key] = value;
+                    string newJsonResult = JsonConvert.SerializeObject(jsObject, Formatting.Indented);
+                    Console.WriteLine(newJsonResult);
+                    File.WriteAllText(filePath, newJsonResult);
+
+                }
+                else
+                {
+                    throw new Exception("Couldn't parsing to object");
+                }
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+
+           
+
+
+        }
+
+        public static void saveObjectToFile(Object obj, string filePath)
+        {
+           
+
+        }
+
+
+
+    }
+}
